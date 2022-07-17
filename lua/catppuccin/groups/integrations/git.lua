08c666cf3a0dacfd664d57e2488b0fd98cf51ccb
Nick Pezza <pezza@hey.com>
2022-07-17 13:22:06 -0400

    Fixes

--- lua/catppuccin/groups/integrations/git.lua
+++ lua/catppuccin/groups/integrations/git.lua
@@ -2,16 +2,16 @@ local M = {}
 
 function M.get()
 	return {
-		gitcommitOverflow = { fg = cp.red },
-        gitcommitUnmerged = { fg = cp.green },
-        gitcommitBranch = { fg = cp.purple },
-        gitcommitDiscardedType = { fg = cp.red },
-        gitcommitSelectedType = { fg = cp.green },
-        gitcommitUntrackedFile = { fg = cp.sky },
-        gitcommitDiscardedFile = { fg = cp.red },
-        gitcommitSelectedFile = { fg = cp.green },
-        gitcommitUnmergedFile = { fg = cp.yellow },
-        gitcommitOverflow = { fg = cp.red },
+    gitcommitOverflow = { fg = cp.red },
+    gitcommitUnmerged = { fg = cp.green },
+    gitcommitBranch = { fg = cp.purple },
+    gitcommitDiscardedType = { fg = cp.red },
+    gitcommitSelectedType = { fg = cp.green },
+    gitcommitUntrackedFile = { fg = cp.sky },
+    gitcommitDiscardedFile = { fg = cp.red },
+    gitcommitSelectedFile = { fg = cp.green },
+    gitcommitUnmergedFile = { fg = cp.yellow },
+    gitcommitOverflow = { fg = cp.red },
 	}
 end
 
--- lua/catppuccin/groups/integrations/treesitter.lua
+++ lua/catppuccin/groups/integrations/treesitter.lua
@@ -8,102 +8,107 @@ function M.get()
 
 	local math_logic = cp.peach
 	return {
-		-- These groups are for the neovim tree-sitter highlights.
-		-- As of writing, tree-sitter support is a WIP, group names may change.
-		-- By default, most of these groups link to an appropriate Vim group,
-		-- TSError -> Error for example, so you do not have to define these unless
-		-- you explicitly want to support Treesitter's improved syntax awareness.
-		TSField = { fg = cp.teal }, -- For fields.
-		TSProperty = { fg = cp.teal, style = cnf.styles.properties or {} }, -- Same as TSField.
+    -- pink
+    TSConditional = { fg = cp.pink },
+    TSException = { fg = cp.pink },
+    TSInclude = { fg = cp.pink },
+    TSKeyword = { fg = cp.pink },
+    TSKeywordFunction = { fg = cp.pink },
+    TSKeywordOperator = { fg = cp.pink },
+    TSRepeat = { fg = cp.pink },
+    TSOperator = { fg = cp.pink },
+    RubyModelMacro = { fg = cp.pink },
+    RubyModelCallback = { fg = cp.pink },
+    RubyModelValidations = { fg = cp.pink },
+    rubyHelper = { fg = cp.pink },
+    rubyViewHelper = { fg = cp.pink },
+    rubyControl = { fg = cp.pink },
+    rubyCurelyBlock = { fg = cp.pink },
 
-		TSInclude = { fg = cp.mauve, style = cnf.styles.keywords or {} }, -- For includes: #include in C, use or extern crate in Rust, or require in Lua.
-		TSOperator = { fg = operators, style = cnf.styles.operators or {} }, -- For any operator: +, but also -> and * in cp.
-		TSKeywordOperator = { fg = cp.mauve, style = cnf.styles.operators or {} }, -- For new keyword operator
-		TSPunctSpecial = { fg = cp.sky, style = cnf.styles.operators or {} }, -- For special punctutation that does not fall in the catagories before.
+    TSFuncMacro = { fg = cp.teal },
 
-		TSFloat = { fg = math_logic, style = cnf.styles.numbers or {} }, -- For floats.
-		TSNumber = { fg = math_logic, style = cnf.styles.numbers or {} }, -- For all numbers
-		TSBoolean = { fg = math_logic, style = cnf.styles.booleans or {} }, -- For booleans.
+    -- yellow
+    TSConstructor = { fg = cp.yellow },
+    TSType = { fg = cp.yellow },
+    TSConstant = { fg = cp.yellow },
+    TSBoolean = { fg = cp.yellow },
+    TSFloat = { fg = cp.yellow },
+    TSNumber = { fg = cp.yellow },
+    TSConstBuiltin = { fg = cp.yellow },
+    TSConstMacro = { fg = cp.yellow },
+    TSTypeBuiltin = { fg = cp.yellow },
+    rubyPredefinedConstant = { fg = cp.yellow },
+    rubyPseudoVariable = { fg = cp.yellow },
+    rubyConstant = { fg = cp.yellow },
+    rubyBoolean = { fg = cp.yellow },
 
-		TSConstructor = { fg = cp.sapphire }, -- For constructor calls and definitions: = { } in Lua, and Java constructors.
-		TSConstant = { fg = cp.peach }, -- For constants
-		TSConditional = { fg = cl, style = cnf.styles.conditionals or {} }, -- For keywords related to conditionnals.
-		TSRepeat = { fg = cl, style = cnf.styles.loops or {} }, -- For keywords related to loops.
-		TSException = { fg = cp.mauve, style = cnf.styles.keywords or {} }, -- For exception related keywords.
+    -- green
+    TSString = { fg = cp.green },
+    TSLiteral = { fg = cp.green },
+    TSLiteral = { fg = cp.green },
+    rubyString = { fg = cp.green },
 
-		-- builtin
-		TSConstBuiltin = { fg = cp.peach, style = cnf.styles.keywords or {} }, -- For constant that are built in the language: nil in Lua.
-		TSFuncBuiltin = { fg = cp.peach, style = cnf.styles.functions or {} }, -- For builtin functions: table.insert in Lua.
+    -- blue
+    TSMethod = { fg = cp.blue },
+    TSFunction = { fg = cp.blue },
 
-		TSNamespace = { fg = cp.blue, style = { "italic" } }, -- For identifiers referring to modules and namespaces.
-		TSType = { fg = cp.yellow, style = cnf.styles.types or {} }, -- For types.
-		TSTypeBuiltin = { fg = cp.yellow, style = cnf.styles.properties or "italic" }, -- For builtin types.
-		TSVariableBuiltin = { fg = cp.red }, -- Variable names that are defined by the languages, like this or self.
+    -- sky
+    TSAttribute = { fg = cp.sky },
+    TSField = { fg = cp.sky },
+    TSFuncBuiltin = { fg = cp.sky }, -- or peach
+    TSProperty = { fg = cp.sky },
+    TSSymbol = { fg = cp.sky },
+    TSStringRegex = { fg = cp.sky },
+    TSStringEscape = { fg = cp.sky }, -- or pink
+    rubySymbol = { fg = cp.sky },
 
-		TSFunction = { fg = cp.blue, style = cnf.styles.functions or {} }, -- For function (calls and definitions).
-		TSFuncMacro = { fg = cp.teal, style = cnf.styles.functions or {} }, -- For macro defined functions (calls and definitions): each macro_rules in Ruscp.
-		TSParameter = { fg = cp.maroon, style = { "italic" } }, -- For parameters of a function.
-		TSKeywordFunction = { fg = cp.mauve, style = cnf.styles.keywords or {} }, -- For keywords used to define a fuction.
-		TSKeyword = { fg = keywords, style = cnf.styles.keywords or {} }, -- For keywords that don't fall in previous categories.
-		TSKeywordReturn = { fg = cp.mauve, style = cnf.styles.keywords or {} },
-		-- TSAnnotation        = { };    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
-		-- TSAttribute         = { };    -- (unstable) TODO: docs
-		-- TSCharacter         = { };    -- For characters.
-		-- TSoverlay0           = { };    -- For overlay0 blocks.
+    -- red
+    TSLabel = { fg = cp.maroon }, -- sapphire
+    TSTag = { fg = cp.red }, -- mauve
+    TSTagDelimiter = { fg = cp.red }, -- sky
+    TSVariableBuiltin = { fg = cp.red },
+    TSGlobalVariable = { fg = cp.red },
+    TSPunctSpecial = { fg = cp.red }, -- sky
+    TSParameter = { fg = cp.red }, -- maroon
+    rubyBlockParameterList = { fg = cp.red },
+
+    -- text
+    TSAnnotation = { fg = cp.text },
+    TSCharacter = { fg = cp.text },
+    TSError = { fg = cp.text },
+    TSNamespace = { fg = cp.text }, -- blue
+    TSNone = { fg = cp.text },
+    TSParameterReference = { fg = cp.text },
+    TSPunctDelimiter = { fg = cp.text }, -- overlay2
+    TSPunctBracket = { fg = cp.rosewater }, -- overlay2
+    TSText = { fg = cp.text },
+    TSStrong = { fg = cp.text, style = "bold" }, -- maroon
+    TSEmphasis = { fg = cp.text, style = "italic" }, -- maroon
+    TSUnderline = { fg = cp.text },
+    TSStrike = { fg = cp.text },
+    TSTitle = { fg = cp.text, style = "bold" }, -- blue
+    TSMath = { fg = cp.text },
+    TSEnviroment = { fg = cp.text },
+    TSEnviromentName = { fg = cp.text },
+		TSKeywordReturn = { fg = cp.mauve, style = cnf.styles.keywords or "NONE" },
 		TSNote = { fg = cp.base, bg = cp.blue },
 		TSWarning = { fg = cp.base, bg = cp.yellow },
 		TSDanger = { fg = cp.base, bg = cp.red },
-		TSConstMacro = { fg = cp.mauve }, -- For constants that are defined by macros: NULL in cp.
-		-- TSError = { fg = cp.red }, -- For syntax/parser errors.
-		-- rustTSField = { fg = cp.surface1 }, -- For fields.
-		TSLabel = { fg = cp.sapphire }, -- For labels: label: in C and :label: in Lua.
-		TSMethod = { fg = cp.blue, style = cnf.styles.functions or {} }, -- For method calls and definitions.
-		-- TSNone              = { };    -- TODO: docs
-		-- TSParameterReference= { };    -- For references to parameters of a function.
 		tomlTSProperty = { fg = cp.blue }, -- Differentiates between string and properties
-		TSPunctDelimiter = { fg = cp.overlay2 }, -- For delimiters ie: .
-		-- TSPunctBracket = { fg = delimeters }, -- For brackets and parenthesis.
-		TSPunctBracket = { fg = cp.overlay2 }, -- For brackets and parenthesis.
-		TSString = { fg = cp.green, style = cnf.styles.strings or {} }, -- For strings.
-		TSStringRegex = { fg = cp.peach, style = cnf.styles.strings or {} }, -- For regexes.
-		-- TSSymbol            = { };    -- For identifiers referring to symbols or atoms.
-		TSVariable = { fg = cp.text, style = cnf.styles.variables or {} }, -- Any variable name that does not have another highlighcp.
-		TSTagAttribute = { fg = cp.teal, style = { "italic" } }, -- Tags like html tag names.
-		TSTag = { fg = cp.mauve }, -- Tags like html tag names.
-		TSTagDelimiter = { fg = cp.sky }, -- Tag delimiter like < > /
-		TSText = { fg = cp.text }, -- For strings considerated text in a markup language.
-		-- TSEmphasis          = { };    -- For text to be represented with emphasis.
-		-- TSUnderline         = { };    -- For text to be represented with an underline.
-		-- TSStrike            = { };    -- For strikethrough texcp.
-		-- TSTitle             = { };    -- Text that is part of a title.
-		-- TSLiteral           = { };    -- Literal texcp.
-		-- TSURI               = { };    -- Any URI like a link or email.
-		--
+		TSTagAttribute = { fg = cp.teal, style = "italic" }, -- Tags like html tag names.
+
+		TSComment = { fg = cp.surface1 },
+
 		-- Markdown tresitter parser support
 		TSURI = { fg = cp.rosewater, style = { "italic", "underline" } }, -- urls, links and emails
-		TSLiteral = { fg = cp.teal, style = { "italic" } }, -- used for inline code in markdown and for doc in python (""")
-		TSTextReference = { fg = cp.lavender, style = { "bold" } }, -- references
-		TSTitle = { fg = cp.blue, style = { "bold" } }, -- titles like: # Example
-		TSEmphasis = { fg = cp.maroon, style = { "italic" } }, -- bold
-		TSStrong = { fg = cp.maroon, style = { "bold" } }, -- italic
-		TSStringEscape = { fg = cp.pink, style = cnf.styles.strings }, -- For escape characters within a string.
-
-		-- bash
-		-- bashTSFuncBuiltin = { fg = cp.red, style = { "italic" } },
-		-- bashTSParameter = { fg = cp.yellow, style = { "italic" } },
+		TSLiteral = { fg = cp.teal, style = "italic" }, -- used for inline code in markdown and for doc in python (""")
+		TSTextReference = { fg = cp.lavender, style = "bold" }, -- references
 
 		-- json
 		jsonTSLabel = { fg = cp.blue }, -- For labels: label: in C and :label: in Lua.
 
 		-- lua
 		luaTSConstructor = { fg = cp.lavender }, -- For constructor calls and definitions: = { } in Lua, and Java constructors.
-
-		-- typescript
-		typescriptTSConstructor = { fg = cp.lavender },
-
-		-- TSX (Typescript React)
-		tsxTSConstructor = { fg = cp.lavender },
-		tsxTSTagAttribute = { fg = cp.mauve },
 
 		-- cpp
 		cppTSProperty = { fg = cp.rosewater },
